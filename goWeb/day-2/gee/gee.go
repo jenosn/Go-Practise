11de567e7489e03c52f358651f869ed86f97c5ae
jenson <jensontu@gmail.com>
2020-04-19 17:53:20 +0800

    新增

--- goWeb/day-2/gee/gee.go
+++ goWeb/day-2/gee/gee.go
@@ -1,20 +1,42 @@
 package gee
 
-import "log"
+import (
+    "log"
+    "net/http"
+)
 
 type HandlerFunc func(ctx *Context)
 
 type Engine struct {
-	route *Route
+    route *Route
 }
 
-func newEngine() *Engine {
-	return &Engine{
-		route: newRoute(),
-	}
+func NewEngine() *Engine {
+    return &Engine{
+        route: newRoute(),
+    }
 }
 
 func (engine *Engine) addRoute(method string, pattern string, handlerFunc HandlerFunc) {
-	log.Printf("Route %4s  %s",method,pattern)
-	engine.route.addRoute(method, pattern, handlerFunc)
+    log.Printf("Route %4s  %s", method, pattern)
+    engine.route.addRoute(method, pattern, handlerFunc)
+}
+
+func (engine *Engine) Get(pattern string, handlerFunc HandlerFunc) {
+    log.Printf("Route %4s", pattern)
+    engine.route.addRoute("GET", pattern, handlerFunc)
+}
+
+func (engine *Engine) Post(pattern string, handlerFunc HandlerFunc) {
+    log.Printf("Route %4s ", pattern)
+    engine.route.addRoute("POST", pattern, handlerFunc)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    c := newContext(w, r)
+    engine.route.handler(c)
+}
+
+func (engine *Engine) Run(add string) {
+    http.ListenAndServe(add, engine)
 }
--- goWeb/day-2/main.go
+++ goWeb/day-2/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+    "Go-Practise/goWeb/day-2/gee"
+    "net/http"
+)
+
 func main() {
+    engine := gee.NewEngine()
+    engine.Get("/", func(ctx *gee.Context) {
+        ctx.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+    })
+    engine.Run(":9999")
 
 }
